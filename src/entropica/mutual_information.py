e91f89cd7789ae0c7d3bb49a98d85cae2d5ca95d
Gabriel Genest <9.genest.gabriel@gmail.com>
2026-08-11 14:37:26 -0400

    Create mutual_information.py
    Public API (subject to change)

---
+++ src/entropica/mutual_information.py
@@ -0,0 +1,29 @@
+from .estimators.knn import KNNMutualInformation
+import cupy as cp
+from cupy.typing import ArrayLike
+
+
+def mutual_information(x: ArrayLike, y: ArrayLike, *, k: int = 3, add_noise: bool = True, noise_intensity: float = 1e-8,
+                       dtype: cp.dtype = cp.float32,
+                       random_state: int | cp.random.Generator | None = None) -> cp.ndarray:
+    estimator = KNNMutualInformation(
+        k=k,
+        add_noise=add_noise,
+        noise_intensity=noise_intensity,
+        dtype=dtype,
+        random_state=random_state,
+    )
+    return estimator.compute(x, y)
+
+
+def pairwise_mutual_information(data: ArrayLike, *, k: int = 3, add_noise: bool = True, noise_intensity: float = 1e-8,
+                                dtype: cp.dtype = cp.float32,
+                                random_state: int | cp.random.Generator | None = None) -> cp.ndarray:
+    estimator = KNNMutualInformation(
+        k=k,
+        add_noise=add_noise,
+        noise_intensity=noise_intensity,
+        dtype=dtype,
+        random_state=random_state,
+    )
+    return estimator.compute_pairwise(data)
